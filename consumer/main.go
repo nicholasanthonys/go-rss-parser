92511bad59366c9fb69a9303912eaeb111e48ad2
Nicholas Anthony S <anthony.nicholas84@gmail.com>
2022-10-23 12:43:46 +0700

    add producer and consumer

---
+++ consumer/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/streadway/amqp"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	amqpConnection, err := amqp.Dial(os.Getenv("RABBITMQ_URI"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer amqpConnection.Close()
+
+	channelAmqp, _ := amqpConnection.Channel()
+	defer channelAmqp.Close()
+
+	forever := make(chan bool)
+
+	msgs, err := channelAmqp.Consume(
+		os.Getenv("RABBITMQ_QUEUE"),
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf("[*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/nicholasanthonys/go-rss-parser/parser
+module github.com/nicholasanthonys/go-rss-parser
 
 go 1.19
 
@@ -24,6 +24,7 @@ require (
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/streadway/amqp v1.0.0
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.1 // indirect
